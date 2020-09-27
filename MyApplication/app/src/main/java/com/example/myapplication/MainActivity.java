8d7d616a27fd93bdfbaadaba768d0cbe1a3ed722
rameenasghar <ramiasghar22@gmail.com>
2020-09-28 04:56:05 +0500

    On click message changes

--- MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,7 +3,10 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +14,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    public void onBtnClick(View view) {
+        TextView text = findViewById(R.id.textMessage);
+        text.setText("Hello Have a wonderful day!");
     }
 }
